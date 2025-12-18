1d75ceaa291866d17dda5ee30dff35cb0b18ba54
gaierda1 <gaierda1@fit.cvut.cz>
2025-12-18 21:52:59 +0100

    Setup connection to db from map-api

--- map-api/src/main/kotlin/Main.kt
+++ map-api/src/main/kotlin/Main.kt
@@ -1,9 +1,16 @@
 package cz.cvut.fit.gaierda1
 
 import cz.cvut.fit.gaierda1.convertor.netex.handleBusLineFile
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
 import java.io.File
 
-fun main() {
+@SpringBootApplication
+class Application
+
+fun main(args: Array<String>) {
     val xmlFile = File("src/data/city_lines/NX-PI-01_CZ_CISJR-JDF_LINE-1_20251022.xml")
     handleBusLineFile(xmlFile)
+
+    runApplication<Application>(*args)
 }
