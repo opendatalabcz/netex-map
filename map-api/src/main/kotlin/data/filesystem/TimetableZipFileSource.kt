de75c3d8c646fb97fe95116d3ad27ae1a76b90b8
gaierda1 <gaierda1@fit.cvut.cz>
2026-04-11 20:59:09 +0200

    Remove unnecessary full netex input file loading into memory before yielding from sequence

--- map-api/src/main/kotlin/data/filesystem/TimetableZipFileSource.kt
+++ map-api/src/main/kotlin/data/filesystem/TimetableZipFileSource.kt
@@ -1,7 +1,6 @@
 package cz.cvut.fit.gaierda1.data.filesystem
 
 import cz.cvut.fit.gaierda1.domain.port.TimetableSourcePort
-import java.io.ByteArrayInputStream
 import java.io.InputStream
 import java.util.zip.ZipInputStream
 import java.util.zip.ZipEntry
@@ -16,9 +15,7 @@ class TimetableZipFileSource(
                 var entry: ZipEntry? = zipInputStream.nextEntry
                 while (entry != null) {
                     if (!entry.isDirectory) {
-                        val streamContent = zipInputStream.readBytes()
-                        yield(ByteArrayInputStream(streamContent))
-                        zipInputStream.closeEntry()
+                        yield(CloseIgnoringInputStream(zipInputStream))
                     }
                     entry = zipInputStream.nextEntry
                 }
