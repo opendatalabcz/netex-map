1d7caeea16a781514bb8a01207c0796fa8340e90
gaierda1 <gaierda1@fit.cvut.cz>
2026-04-18 20:53:32 +0200

    Remove unplugin-fonts dependency

--- map/src/main.ts
+++ map/src/main.ts
@@ -2,6 +2,5 @@ import { createApp } from 'vue'
 import App from '@/App.vue'
 import Vuetify from '@/plugins/vuetify'
 import I18n from '@/plugins/i18n/i18n'
-import 'unfonts.css'
 
 createApp(App).use(I18n).use(Vuetify).mount('#app')
--- map/vite.config.ts
+++ map/vite.config.ts
@@ -1,7 +1,6 @@
 import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import ViteFonts from 'unplugin-fonts/vite'
 import vuetify from 'vite-plugin-vuetify'
 
 // https://vite.dev/config/
@@ -11,17 +10,6 @@ export default defineConfig({
   plugins: [
     vue(),
     vuetify(),
-    ViteFonts({
-      fontsource: {
-        families: [
-          {
-            name: 'Roboto',
-            weights: [100, 300, 400, 500, 700, 900],
-            styles: ['normal'], // can add 'italic'
-          },
-        ],
-      },
-    }),
   ],
   resolve: {
     alias: {
