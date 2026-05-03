4710304936be3c7d10efcc6edded188557ac8ebd
gaierda1 <gaierda1@fit.cvut.cz>
2026-05-03 09:40:51 +0200

    Add rules to name normalisation

--- map-api/src/main/kotlin/domain/usecase/load/NormalizeStopName.kt
+++ map-api/src/main/kotlin/domain/usecase/load/NormalizeStopName.kt
@@ -7,13 +7,17 @@ class NormalizeStopName: NormalizeStopNameUseCase {
     private fun String.collapseCommas(): String = replace(Regex("""\s*,(?:\s*,)*\s*"""), ",")
     private fun String.removeSquareBrackets(): String = replace(Regex("""\s*\[[^]]*]"""), "")
     private fun String.addSpaceAfterDots(): String = replace(Regex("""\.(?:\s+|([^,]))"""), ". $1")
+    private fun String.replaceUnbreakableSpaces(): String = replace("\u00A0", " ")
     private fun String.removeTrailingComma(): String = if (endsWith(",")) substring(0, length - 1) else this
+    private fun String.removeXInCircle(): String = replace(Regex("""Ⓧ\s*"""), "")
 
     override fun normalize(stopName: String): String {
         return stopName
             .collapseCommas()
             .removeSquareBrackets()
             .addSpaceAfterDots()
+            .replaceUnbreakableSpaces()
+            .removeXInCircle()
             .removeTrailingComma()
     }
 }
